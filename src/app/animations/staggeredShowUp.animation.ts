a2e452989621d7336c047bea19db8731c8dee4b0
Jlima2016306 <jlima-2016306@kinal.edu.gt>
2021-09-01 17:41:34 -0600

    Animaciones

--- src/app/animations/staggeredShowUp.animation.ts
+++ src/app/animations/staggeredShowUp.animation.ts
@@ -5,7 +5,7 @@ export const staggeredShowUp = trigger('staggeredShowUp',[
   transition('* => *',[
     query(':enter', style({transform:"translateX(-100px)", opacity:'0'}), {optional:true}),
       query(':enter', stagger('80ms',[
-      animate('0.4s ease-out', keyframes([
+      animate('0.4s ease-in', keyframes([
         style({transform:"translateX(-100px)", opacity:'0',offset:0}),
         style({transform:"translateX(50px)", opacity:'1',offset:0.5}),
         style({transform:"translateX(0px)", opacity:'1',offset:1})
